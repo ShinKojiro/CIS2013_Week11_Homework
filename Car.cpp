5a37c5adc70a2efe4ccddc7a43ca0c888334afb7
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2019-04-16 15:20:24 -0500

    Create Car.cpp

---
+++ Car.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+class Car(){
+
+private:
+
+    string color;
+    string make;
+    string model;
+    int year;
+    int currentSpeed;
+    bool is_starter;
+    bool is_moving;
+
+public:
+
+
+
+};
